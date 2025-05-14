286730d1028b7ba1be8a234fbe975c5c57c0236b
harry <harry@harryhart.dev>
2025-05-14 22:40:21 +0100

    For some reason ref sdp-5-small missing

--- mupq/crypto_sign/crossv2.0-sha3-r-sdp-1-small/light/CROSS.c
+++ mupq/crypto_sign/crossv2.0-sha3-r-sdp-1-small/light/CROSS.c
@@ -741,13 +741,13 @@ void CROSS_sign(const sk_t *SK, const char *const m, const uint64_t mlen,
   unsigned char round_seeds[T * SEED_LENGTH_BYTES] = {0};
   // Limit scope for seed_tree
 #if defined(OPT_GGM)
-  //{
+  {
 #endif
-  uint8_t seed_tree[SEED_LENGTH_BYTES * NUM_NODES_SEED_TREE] = {0};
-  gen_seed_tree(seed_tree, root_seed, sig->salt);
-  seed_leaves(round_seeds, seed_tree);
+    uint8_t seed_tree[SEED_LENGTH_BYTES * NUM_NODES_SEED_TREE] = {0};
+    gen_seed_tree(seed_tree, root_seed, sig->salt);
+    seed_leaves(round_seeds, seed_tree);
 #if defined(OPT_GGM)
-  //}
+  }
 #endif
 #endif
 
