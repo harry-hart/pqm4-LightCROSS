7afc09dc91e0819be5257892b8f840e1f92a63c9
harry <harry@harryhart.dev>
2025-06-10 16:30:25 +0100

    Work on profiling script

---
+++ profile.py
@@ -0,0 +1,35 @@
+from mupq import mupq
+from interface import parse_arguments, get_platform
+import sys
+
+def main():
+    args, rest = parse_arguments()
+    platform, settings = get_platform(args)
+    with platform:
+        
+        schemes = [s for s in rest if s not in ['--nostack',
+                                                '--nospeed',
+                                                '--nohashing',
+                                                '--nosize']]
+        if "--nostack" not in rest:
+            test = mupq.StackBenchmark(settings, platform)
+            if test.test_all(schemes):
+                sys.exit(1)
+
+        if "--nospeed" not in rest:
+            test = mupq.SpeedBenchmark(settings, platform)
+            if test.test_all(schemes):
+                sys.exit(1)
+
+        if "--nohashing" not in rest:
+            test = mupq.HashingBenchmark(settings, platform)
+            if test.test_all(schemes):
+                sys.exit(1)
+
+        if "--nosize" not in rest:
+            test = mupq.SizeBenchmark(settings, platform)
+            if test.test_all(schemes):
+                sys.exit(1)
+
+if __name__=="__main__":
+    main()
