68be871be0d6521a96f6778e8fff713a6ff59945
Harry Hart <h.hart@surrey.ac.uk>
2025-07-14 17:01:31 +0100

    Cleaning up repo

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -1,3 +1,28 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-python3 ./profile.py -t 30 -p nucleo-l4r5zi -s "crossv2.0"
+if [[ $# -eq 0 ]]; then
+  echo "Please provide elf file for gdb"
+  exit -1
+fi
+
+nsamples=10
+sleeptime=0
+#pid=$(pidof mysqld)
+elf=$1
+
+for x in $(seq 1 $nsamples)
+  do
+    arm-none-eabi-gdb \
+      -ex "set pagination 0" \
+      -ex "target extended-remote :3333" \
+      -ex "thread apply all bt" \
+      -batch \
+      $elf
+    sleep $sleeptime
+  done | \
+awk '
+  BEGIN { s = ""; } 
+  /^Thread/ { print s; s = ""; } 
+  /^\#/ { if (s != "" ) { s = s "," $4} else { s = $4 } } 
+  END { print s }' | \
+sort | uniq -c | sort -r -n -k 1,1
